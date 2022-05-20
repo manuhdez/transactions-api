53bfb62bca1f432bdedcf79df85f2cc34d33107d
Manuel Hernández <manuel.hita@ukio.com>
2022-05-20 01:18:57 +0100

    feat(account): turn account property private

--- internal/accounts/domain/account/account.go
+++ internal/accounts/domain/account/account.go
@@ -1,10 +1,18 @@
 package account
 
 type Account struct {
-	Id      string
-	Balance float32
+	id      string
+	balance float32
 }
 
 func New(id string, balance float32) Account {
 	return Account{id, balance}
+}
+
+func (a Account) Id() string {
+	return a.id
+}
+
+func (a Account) Balance() float32 {
+	return a.balance
 }

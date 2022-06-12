563599810167cb25d41082b109f64a5778854086
Manuel Hernández <hi@manuhdez.com>
2022-06-12 03:26:50 +0100

    fix(transactions): update sql schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `transactions` (
     `id` INT auto_increment NOT NULL,
     `account_id` varchar (100) NOT NULL,
     `amount` float NOT NULL,
-    `balance` float NOT NULL,
+    `currency` varchar(3) NOT NULL,
     `type` enum ('deposit', 'withdrawal') NOT NULL,
     `date` timestamp NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY (`id`)

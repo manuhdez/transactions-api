c5c10ff4dc2f2e1853b491a2786876e5e4514873
Manuel Hernández <hi@manuhdez.com>
2022-06-12 03:28:00 +0100

    feat(transactions): implement deposit method on mysql repository

--- internal/transactions/infra/transaction_mysql_repository.go
+++ internal/transactions/infra/transaction_mysql_repository.go
@@ -3,7 +3,6 @@ package infra
 import (
 	"context"
 	"database/sql"
-	"fmt"
 
 	"github.com/manuhdez/transactions-api/internal/transactions/domain/transaction"
 )
@@ -16,7 +15,12 @@ func NewTransactionMysqlRepository(db *sql.DB) TransactionMysqlRepository {
 	return TransactionMysqlRepository{db: db}
 }
 
-func (r TransactionMysqlRepository) Deposit(ctx context.Context, transaction transaction.Transaction) error {
-	fmt.Println("Deposit", transaction)
+func (r TransactionMysqlRepository) Deposit(ctx context.Context, t transaction.Transaction) error {
+	query := "insert into transactions (account_id, amount, currency, type) values (?, ?, ?, ?)"
+	_, err := r.db.ExecContext(ctx, query, t.AccountId, t.Amount, t.Currency, t.Type)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }

b3b83f883fa4366693bea4df6aaedab0612aa452
Manuel Hernández <manuel.hita@ukio.com>
2023-01-24 08:44:44 +0000

    feat(accounts): update account created event

--- internal/accounts/domain/event/account_created.go
+++ internal/accounts/domain/event/account_created.go
@@ -1,8 +1,24 @@
 package event
 
-type AccountCreated struct{}
+import "encoding/json"
+
+type AccountCreated struct {
+	body []byte
+}
+
+type AccountCreatedBody struct {
+	Type    string  `json:"type"`
+	Id      string  `json:"id"`
+	Balance float32 `json:"balance"`
+}
 
 var AccountCreatedType Type = "event.accounts.account_created"
+
+func NewAccountCreated(id string, balance float32) AccountCreated {
+	body := AccountCreatedBody{string(AccountCreatedType), id, balance}
+	b, _ := json.Marshal(body)
+	return AccountCreated{b}
+}
 
 func (a AccountCreated) Type() Type {
 	return AccountCreatedType

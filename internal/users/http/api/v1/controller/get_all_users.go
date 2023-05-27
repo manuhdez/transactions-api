d6cadb5a3cd252dd0649638aed1eb4bf6b4e8086
Manuel Hernández <hi@manuhdez.com>
2023-05-27 01:02:34 +0100

    feat(users): get all users controller

---
+++ internal/users/http/api/v1/controller/get_all_users.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/manuhdez/transactions-api/internal/users/application/service"
+	"github.com/manuhdez/transactions-api/internal/users/domain/user"
+	"github.com/manuhdez/transactions-api/internal/users/infra"
+)
+
+type GetAllUsers struct {
+	retriever service.UsersRetriever
+}
+
+func NewGetAllUsersController(retriever service.UsersRetriever) GetAllUsers {
+	return GetAllUsers{
+		retriever: retriever,
+	}
+}
+
+func (ctlr GetAllUsers) Handle() (w http.ResponseWriter, r *http.Request) {
+	users, err := ctlr.retriever.Retrieve()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if len(users) == 0 {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("[]"))
+		return
+	}
+
+	data := getResponseData(users)
+	response, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(response)
+	return
+}
+
+func getResponseData(users []user.User) []infra.UserJson {
+	var res []infra.UserJson
+	for _, user := range users {
+		res = append(res, infra.NewUserJson(user))
+	}
+	return res
+}

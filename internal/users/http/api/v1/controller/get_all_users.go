3f2ff63ffd49f9a25b5c4eec176f31a17e39e1b8
Manuel Hernández <hi@manuhdez.com>
2023-07-02 17:49:54 +0100

    refactor: set content-type header to json

--- internal/users/http/api/v1/controller/get_all_users.go
+++ internal/users/http/api/v1/controller/get_all_users.go
@@ -39,6 +39,7 @@ func (ctlr GetAllUsers) Handle(w http.ResponseWriter, _ *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write(response)
 	return

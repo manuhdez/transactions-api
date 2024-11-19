5f0d38925d8482fd92fed9c0d2b1c5592a95cee3
Manuel Hernández <hi@manuhdez.com>
2024-11-19 13:00:57 +0000

    feat: display transaction date in json output

--- internal/transactions/infra/transaction_json.go
+++ internal/transactions/infra/transaction_json.go
@@ -1,15 +1,23 @@
 package infra
 
 import (
+	"time"
+
 	"github.com/manuhdez/transactions-api/internal/transactions/domain/transaction"
 )
 
 type JsonTransaction struct {
-	Account string  `json:"account_id"`
-	Type    string  `json:"type"`
-	Amount  float32 `json:"amount"`
+	Account string    `json:"account_id"`
+	Type    string    `json:"type"`
+	Amount  float32   `json:"amount"`
+	Date    time.Time `json:"date"`
 }
 
 func NewJsonTransaction(t transaction.Transaction) JsonTransaction {
-	return JsonTransaction{t.AccountId, string(t.Type), t.Amount}
+	return JsonTransaction{
+		Account: t.AccountId,
+		Type:    string(t.Type),
+		Amount:  t.Amount,
+		Date:    t.Date,
+	}
 }
